6e95317774d17304f8d9e7a6cabe2d152b2ed09a
Gabriel Oliveira <gabriel.oliveira@etu.univ-smb.fr>
2023-01-26 09:20:12 +0100

    Codage GetAll

---
+++ WSConvertisseur/Controllers/DevisesController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WSConvertisseur.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WSConvertisseur.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DevisesController : ControllerBase
+    {
+        public List<Devise> listDevises = new List<Devise>();
+
+        public DevisesController()
+        {
+            listDevises.Add(new Devise(1, "Dollar", 1.08));
+            listDevises.Add(new Devise(2, "Franc Suisse", 1.07));
+            listDevises.Add(new Devise(3, "Yen", 120));
+
+
+
+        }
+
+
+
+
+
+
+        // GET: api/<DevisesController>
+        [HttpGet]
+        public IEnumerable<Devise> GetAll()
+        {
+            return listDevises;
+        }
+
+        // GET api/<DevisesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<DevisesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<DevisesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<DevisesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
