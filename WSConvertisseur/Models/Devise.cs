6b1c7e5535f5f0893564baa474117b61340a2a3e
Gabriel Oliveira <gabriel.oliveira@etu.univ-smb.fr>
2023-01-26 08:56:00 +0100

    Ajout du modèle

---
+++ WSConvertisseur/Models/Devise.cs
@@ -0,0 +1,62 @@
+﻿namespace WSConvertisseur.Models
+{
+    public class Devise
+    {
+
+        private int idDevise;
+        private string? nomDevise;
+        private double tauxDevise;
+
+
+        public Devise()
+        {
+
+        }
+
+        public Devise(int idDevise, string? nomDevise, double tauxDevise)
+        {
+            this.IdDevise = idDevise;
+            this.NomDevise = nomDevise;
+            this.TauxDevise = tauxDevise;
+        }
+
+        public int IdDevise
+        {
+            get
+            {
+                return idDevise;
+            }
+
+            set
+            {
+                idDevise = value;
+            }
+        }
+
+        public string? NomDevise
+        {
+            get
+            {
+                return nomDevise;
+            }
+
+            set
+            {
+                nomDevise = value;
+            }
+        }
+
+        public double TauxDevise
+        {
+            get
+            {
+                return tauxDevise;
+            }
+
+            set
+            {
+                tauxDevise = value;
+            }
+        }
+    }
+}
