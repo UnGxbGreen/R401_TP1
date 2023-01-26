1f9687669bf336dcaed94e9c89839005a0ae6c67
Gabriel Oliveira <gabriel.oliveira@etu.univ-smb.fr>
2023-01-26 10:55:04 +0100

    add path xml

--- WSConvertisseur/Program.cs
+++ WSConvertisseur/Program.cs
@@ -5,7 +5,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(
+    doc =>
+    {
+        var xmlFile = Path.ChangeExtension(typeof(Program).Assembly.Location, ".xml");
+        doc.IncludeXmlComments(xmlFile);
+    }
+    );
+
 
 var app = builder.Build();
 
