c699a5d5db3151872974b73a4b5bdc90ce1bf7a6
Gabriel Oliveira <gabriel.oliveira@etu.univ-smb.fr>
2023-01-27 13:09:52 +0100

    Test pas tout fait

--- WSConvertisseurTests/Controllers/DevisesControllerTests.cs
+++ WSConvertisseurTests/Controllers/DevisesControllerTests.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WSConvertisseur.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace WSConvertisseur.Controllers.Tests
 {
@@ -55,6 +56,44 @@ namespace WSConvertisseur.Controllers.Tests
             // Assert
             Assert.IsInstanceOfType(result, typeof(IEnumerable<Devise>), "Pas un IEnumerable"); // Test du type de retour
             CollectionAssert.AreEqual(result.ToList(), controller.listDevises, "Les listes ne sont pas les mêmes"); //Test de l'erreur
+        }
+
+        [TestMethod]
+        public void Post_ValidObject()
+        {
+
+            // Arrange
+            DevisesController controller = new DevisesController();
+            // Act
+
+            var result = controller.Post(new Devise(4, "clement", 15));
+            // Assert
+
+
+            Assert.IsInstanceOfType(result, typeof(ActionResult<Devise>), "Pas un ActionResult"); // Test du type de retour
+            Assert.IsInstanceOfType(result.Result, typeof(CreatedAtRouteResult), "pas de type CreatedAtRouteResult"); //Test de l'erreur
+            CreatedAtRouteResult routeResult = (CreatedAtRouteResult)result.Result;
+            Assert.AreEqual(StatusCodes.Status201Created, routeResult.StatusCode, "pas un StatusCode");
+            Assert.AreEqual(routeResult.Value, new Devise(4, "clement", 15), "Pas la bonne valeur");
+        }
+
+        [TestMethod]
+        public void Post_InvalidObject()
+        {
+
+            // Arrange
+            DevisesController controller = new DevisesController();
+            // Act
+
+            var result = controller.Post(new Devise(4, null, 15));
+            // Assert
+
+
+            Assert.IsInstanceOfType(result, typeof(ActionResult<Devise>), "Pas un ActionResult"); // Test du type de retour
+            Assert.IsInstanceOfType(result.Result, typeof(CreatedAtRouteResult), "pas de type CreatedAtRouteResult"); //Test de l'erreur
+            CreatedAtRouteResult routeResult = (CreatedAtRouteResult)result.Result;
+            Assert.AreEqual(StatusCodes.Status201Created, routeResult.StatusCode, "pas un StatusCode");
+            Assert.AreEqual(routeResult.Value, new Devise(4, null, 15), "Pas la bonne valeur");
         }
     }
 }
